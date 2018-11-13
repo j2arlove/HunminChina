b1dfef4da0d146581232a5c36e31cd322e7f945b
Suin Kim <suin.kim@gmail.com>
2018-11-13 19:45:52 +0900

    feat: migrate to react

---
+++ hunmin/src/App.js
@@ -0,0 +1,483 @@
+import React, { Component } from "react";
+import "./App.css";
+import combineOldHangul from "./constants/combineOldHangul";
+import keyCode from "./constants/keyCode";
+import keyCodeWithShift from "./constants/keyCodeWithShift";
+import keyToIntonation from "./constants/keyToIntonation";
+import nonUnicodeToUnicode from "./constants/nonUnicodeToUnicode";
+import specialCharacters from "./constants/specialCharacters";
+
+class App extends Component {
+  state = {
+    isControlPressed: false,
+    isShiftPressed: false,
+    state: 0,
+    typedCharacters: [], // 지금까지 입력한 문자들
+    currentTypingCharacters: {
+      // 현재 입력중인 문자
+      intonation: 0, // 성조
+      keys: [], // 글쇠
+      states: [], // state 히스토리
+      combinedHangul: ""
+    }
+  };
+
+  componentDidMount() {
+    document.onkeydown = this.onKeyDown;
+    document.onkeyup = this.onKeyUp;
+    this.registerStates();
+  }
+
+  getPressedKey = e => {
+    // 눌린 키코드로 어떤 키가 눌렸는지 반환하는 함수
+    // Shift를 같이 눌렀을 때 어떤 키가 눌렸는 지 자동으로 처리
+    if (e.keyCode in keyCode) {
+      if (keyCode[e.keyCode] === "shift") {
+        return keyCode[e.keyCode];
+      } else if (this.state.isShiftPressed && e.keyCode in keyCodeWithShift) {
+        return keyCodeWithShift[e.keyCode];
+      } else if (e.keyCode in keyCode) {
+        return keyCode[e.keyCode];
+      }
+    }
+    return null;
+  };
+
+  onKeyDown = e => {
+    // 키가 내려갈 때 불리는 함수
+    var pressedKey = this.getPressedKey(e);
+    if (pressedKey === "shift") {
+      this.setState({ isShiftPressed: true });
+    } else if (pressedKey === "backspace") {
+      this.backspace();
+    } else if (
+      this.includes(
+        ["up arrow", "left arrow", "right arrow", "down arrow"],
+        pressedKey
+      )
+    ) {
+      const currentTypingCharacters = { ...this.state.currentTypingCharacters };
+      currentTypingCharacters.intonation = keyToIntonation[pressedKey];
+      this.setState({ currentTypingCharacters });
+    } else if (pressedKey === "enter") {
+      this.enter();
+    } else if (pressedKey !== null) {
+      this.state.states[this.state.state](pressedKey);
+    }
+  };
+
+  onKeyUp = e => {
+    // 키가 올라갈 때 불리는 함수
+    var pressedKey = this.getPressedKey(e);
+    if (pressedKey === "shift") {
+      // shift 키 눌림 해제
+      this.setState({ isShiftPressed: false });
+    }
+  };
+
+  includes = (keys, key) => {
+    return keys.indexOf(key) !== -1;
+  };
+
+  translateToUnicode = combinedHangul => {
+    var index = 0;
+    var stage = 0;
+    var unicodeString = "";
+
+    for (stage = 1; stage <= 3; stage++) {
+      var indexCharacter = combinedHangul[index];
+
+      if (this.includes(specialCharacters, indexCharacter)) {
+        unicodeString += indexCharacter;
+        index++;
+      }
+
+      if (stage === 1) {
+        // 초성
+        if (indexCharacter in nonUnicodeToUnicode.cho) {
+          unicodeString += nonUnicodeToUnicode.cho[indexCharacter];
+          index++;
+        }
+      } else if (stage === 2) {
+        // 중성
+        if (indexCharacter in nonUnicodeToUnicode.jung) {
+          unicodeString += nonUnicodeToUnicode.jung[indexCharacter];
+          index++;
+        }
+      } else if (stage === 3) {
+        // 종성
+        if (indexCharacter in nonUnicodeToUnicode.jong) {
+          unicodeString += nonUnicodeToUnicode.jong[indexCharacter];
+          index++;
+        }
+      }
+    }
+    return unicodeString;
+  };
+
+  enter = () => {
+    this.finishCharacter();
+    // chat_contents += "<div class='bubble'>{0}</div>".format(flushOutput());
+    // $(".chat").html(chat_contents);
+    // $(".typed-characters").html("");
+    // env.typedCharacters.splice(0, env.typedCharacters.length);
+    this.transit(0);
+  };
+
+  transit = nextState => {
+    this.setState({ state: nextState });
+  };
+
+  addToCurrentCharacter = key => {
+    const currentTypingCharacters = { ...this.state.currentTypingCharacters };
+
+    currentTypingCharacters.keys.push(key);
+    currentTypingCharacters.states.push(this.state.state);
+
+    this.setState({ currentTypingCharacters });
+  };
+
+  backspace = () => {
+    if (this.state.currentTypingCharacters.keys.length === 0) {
+      // 현재 입력중인 글자가 없을때
+      if (this.state.typedCharacters.length > 0) {
+        // 이미 입력된 글자가 있을때
+        const typedCharacters = [...this.state.typedCharacters];
+        typedCharacters.pop();
+        this.setState({ typedCharacters }); // 한 자를 지움
+      }
+    } else {
+      const currentTypingCharacters = { ...this.state.currentTypingCharacters };
+      currentTypingCharacters.keys.pop(); // 한 글쇠를 지움
+      const prevState = currentTypingCharacters.states.pop(); // 한 state를 지움
+      this.setState({
+        currentTypingCharacters,
+        state: prevState
+      }); // 이전 state로 되돌림
+    }
+  };
+
+  combineHangul = currentCharacter => {
+    currentCharacter.combinedHangul = currentCharacter.keys.join("");
+    for (let combination of Object.keys(combineOldHangul)) {
+      currentCharacter.combinedHangul = currentCharacter.combinedHangul.replace(
+        combination,
+        combineOldHangul[combination]
+      );
+    }
+  };
+
+  finishCharacter = () => {
+    this.combineHangul(this.state.currentTypingCharacters);
+
+    const typedCharacters = [...this.state.typedCharacters];
+    typedCharacters.push(this.state.currentTypingCharacters);
+    this.setState({ typedCharacters });
+
+    this.setState({
+      currentTypingCharacters: {
+        intonation: 0,
+        keys: [],
+        states: [],
+        combinedHangul: ""
+      }
+    });
+  };
+
+  removeOneCurrentKey = () => {
+    const currentTypingCharacters = { ...this.state.currentTypingCharacters };
+    currentTypingCharacters.states.pop();
+    const returnKey = currentTypingCharacters.keys.pop();
+
+    this.setState({ currentTypingCharacters });
+
+    return returnKey;
+  };
+
+  registerStates = () => {
+    const states = {};
+    states[0] = key => {
+      if (this.includes("ㅈㅅㄹㅍ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(1);
+      } else if (this.includes(["ㅈㅇ", "ㅊㅎ", "ㅅㅇ", "ㄹㅇ", "ㅍㅇ"], key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(3);
+      } else if (this.includes("ㄱㄴㄷㅁㅂㅅㅇㅋㅌㅎㄲㄸㅃㅉㅆ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(3);
+      } else if (this.includes(["ㅊ"], key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(4);
+      } else if (this.includes("ㅏㅑㅓㅕㅛㅠㅣㅐㅔㅒㅖ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(9);
+      } else if (this.includes("ㅗ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(10);
+      } else if (this.includes("ㅜ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(11);
+      } else if (this.includes("ㅡ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(12);
+      } else {
+        this.addToCurrentCharacter(key);
+        this.finishCharacter();
+        this.transit(0);
+      }
+    };
+
+    states[1] = key => {
+      if (this.includes("ㅇ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(3);
+      } else if (this.includes("ㅏㅑㅓㅕㅛㅠㅣㅐㅔㅒㅖ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(2);
+      } else if (this.includes("ㅗ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(5);
+      } else if (this.includes("ㅜ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(6);
+      } else if (this.includes("ㅡ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(7);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[3] = key => {
+      if (
+        this.includes(
+          [
+            "ㄱ",
+            "ㄴ",
+            "ㄷ",
+            "ㄹ",
+            "ㅁ",
+            "ㅂ",
+            "ㅅ",
+            "ㅇ",
+            "ㅈ",
+            "ㅊ",
+            "ㅋ",
+            "ㅌ",
+            "ㅍ",
+            "ㅎ",
+            "ㅈㅇ",
+            "ㅊㅎ",
+            "ㅅㅇ",
+            "ㄹㅇ",
+            "ㅍㅇ"
+          ],
+          key
+        )
+      ) {
+        this.finishCharacter();
+        this.transit(0);
+        states[this.state.state](key);
+      } else if (this.includes("ㅏㅑㅓㅕㅛㅠㅣㅐㅔㅒㅖ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(2);
+      } else if (this.includes("ㅗ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(5);
+      } else if (this.includes("ㅜ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(6);
+      } else if (this.includes("ㅡ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(7);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[4] = key => {
+      if (this.includes("ㅎ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(3);
+      } else if (this.includes("ㅏㅑㅓㅕㅛㅠㅣㅐㅔㅒㅖ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(2);
+      } else if (this.includes("ㅗ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(5);
+      } else if (this.includes("ㅜ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(6);
+      } else if (this.includes("ㅡ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(7);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[2] = key => {
+      if (this.includes("ㄴㄹㅇ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(14);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[5] = key => {
+      if (this.includes("ㄴㄹㅇ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(14);
+      } else if (this.includes("ㅏㅐㅣ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(8);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[6] = key => {
+      if (this.includes("ㄴㄹㅇ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(14);
+      } else if (this.includes("ㅓㅣㅔ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(8);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[7] = key => {
+      if (this.includes("ㄴㄹㅇ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(14);
+      } else if (this.includes("ㅣ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(8);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[8] = key => {
+      if (this.includes("ㄴㄹㅇ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(14);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[9] = key => {
+      this.finishCharacter();
+      this.transit(0);
+      this.state.states[this.state.state](key);
+    };
+
+    states[10] = key => {
+      if (this.includes("ㅏㅐㅣ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(13);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[11] = key => {
+      if (this.includes("ㅓㅣㅔ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(13);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[12] = key => {
+      if (this.includes("ㅣ", key)) {
+        this.addToCurrentCharacter(key);
+        this.transit(13);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    states[13] = key => {
+      this.finishCharacter();
+      this.transit(0);
+      this.state.states[this.state.state](key);
+    };
+
+    states[14] = key => {
+      if (this.includes("ㅏㅑㅓㅕㅛㅠㅣㅐㅔㅒㅖㅗㅜㅡ", key)) {
+        var prevKey = this.removeOneCurrentKey();
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](prevKey);
+        this.state.states[this.state.state](key);
+      } else {
+        this.finishCharacter();
+        this.transit(0);
+        this.state.states[this.state.state](key);
+      }
+    };
+
+    this.setState({ states });
+  };
+
+  render() {
+    const typedCharacters = this.state.typedCharacters.map(character => {
+      return (
+        <span className={`intonation-${character.intonation}`}>
+          {this.translateToUnicode(character.combinedHangul)}
+        </span>
+      );
+    });
+
+    const currentCharacter = (
+      <span
+        className={`intonation-${
+          this.state.currentTypingCharacters.intonation
+        }`}
+      >
+        {this.state.currentTypingCharacters.keys.join("")}
+      </span>
+    );
+
+    return (
+      <div className="App">
+        <div className="typed-characters">
+          {typedCharacters}
+          <div className="typing-character">{currentCharacter}</div>
+        </div>
+        <div className="status">State: {this.state.state}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hunmin/src/constants/keyToIntonation.js
@@ -0,0 +1,6 @@
+export default {
+  "right arrow": 1,
+  "up arrow": 2,
+  "left arrow": 3,
+  "down arrow": 4
+};
